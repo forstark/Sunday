71d04a77bf96644d70414c91aba2c1cc519cf0af
forstark <julien.ducornait@gmail.com>
2021-03-28 21:46:48 +0200

    Adding MQTT support

--- Sunday_Master/Sunday_Master.ino
+++ Sunday_Master/Sunday_Master.ino
@@ -1,8 +1,9 @@
 #include <WiFi.h>
+#include <PubSubClient.h>
 
 #define BAUDRATE 115200
 
-const String nom = "IOT_Master";
+const String nom = "Sunday_Master";
 
 const int MASTER_PORT = 4000;
 WiFiServer server(MASTER_PORT);
@@ -15,11 +16,20 @@ WiFiClient slave1;
 IPAddress slave1Address(192, 168, 1, 101);
 const int slave1Port = 5001;
 
+//const char *mqttServer = "iot.fr-par.scw.cloud";
+IPAddress mqttServer(51, 159, 24, 174);
+const int mqttPort = 1883;
+const char *mqttUser = "5913f12c-bdef-48dd-a6b6-e0527d9e6f13";
+const char *mqttPassword = "";
+WiFiClient mqttClient;
+PubSubClient mqttPubClient(mqttClient);
+
 const char* ssid = "Freebox-53DBE7";
 const char* password = "cd5=GVnbAEW)";
 IPAddress ip(192, 168, 1, 100);
 IPAddress gateway(192, 168, 1, 1);
 IPAddress subnet(255, 255, 255, 0);
+IPAddress dns(192, 168, 1, 1);
 
 const int pinButton = 4;
 #define DEBOUNCE_TIME 250
@@ -29,7 +39,7 @@ void setup(void)
 {
   Serial.begin(BAUDRATE);
 
-  WiFi.config(ip, gateway, subnet);
+  WiFi.config(ip, dns, gateway, subnet);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) 
   {
@@ -42,11 +52,33 @@ void setup(void)
 
   server.begin();
 
+  Serial.println("Configuring MQTT Server !");
+  mqttPubClient.setServer(mqttServer, mqttPort);
+  while(!mqttPubClient.connected())
+  {
+    Serial.println("Connecting to MQTT Server ...");
+    if(mqttPubClient.connect(nom.c_str(), mqttUser, mqttPassword))
+    {
+      Serial.println("Connected !");
+    }
+
+    else
+    {
+      Serial.print("Failed with state : ");
+      Serial.println(mqttPubClient.state());
+      delay(2000);
+    }
+  }
+
   pinMode(pinButton, INPUT);
+
+  delay(1000);
+  Serial.println("Ready !");
 }
 
 void loop(void)
-{  
+{ 
+  mqttPubClient.loop();
   clientRequest();
   
   if(digitalRead(pinButton) == HIGH)
@@ -87,6 +119,8 @@ void clientRequest(void)
 void handleRequest(String r)
 {
   Serial.println("Sending data...");
+
+  mqttPubClient.publish("room/heater", r.substring(r.indexOf(":") + 1).c_str());
     
   if(slave1.connect(slave1Address, slave1Port))
   {
